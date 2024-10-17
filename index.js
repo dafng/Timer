aebb9d493e4805e7f907e345e4dd79eaa7a655f2
Dafni Gk <dafnigkortsi@gmail.com>
2024-10-17 13:46:00 +0300

    fixed the basic Countdown function

--- index.js
+++ index.js
@@ -12,26 +12,18 @@ const pauseBtn = document.getElementById('pauseBtn');
 pauseBtn.addEventListener("click", clearInterval);
 //const a = setInterval(CountDown, 1000);
 startBtn.addEventListener("click", setInterval(CountDown, 1000));
-
- 
-
 */
+
 function CountDown(seconds){
     timer.textContent = seconds;
-    seconds--;
     if (seconds < 0) {
         clearInterval(intervalId);
         timer.textContent = "Time's up!";
     }
 } 
-let seconds = 10; 
-const intervalId = setInterval(() => CountDown(seconds--), 1000, 10);
+let seconds = 5; 
+const intervalId = setInterval(() => CountDown(seconds--), 1000); //CountDown(seconds--) first the function is called with the value seconds and after it is called seconds-- is performed
 
-// timer.textContent = i
+
 /* NOTES! 
-CountUp function doesn't work, because the program does not wait
-for setTimeout function to complete. By the time it completes, the 
-for loop is finished and thus i equals 10, which is what is printed 
-on the screen. 
-To bypass this I have to use and watch asychronous functions video on
-Bro Code */
+ */
