430493de66885439a979a7e1dd0abdf98e6b4eac
Dafni Gk <dafnigkortsi@gmail.com>
2024-10-17 14:07:04 +0300

    countdown starts on button click

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 let num_rounds= 19;
 let timerStretch = 33;
 let timerRest = 10;
+let intervalId;
 
 const timer = document.getElementById('timer');
 const rounds = document.getElementById('rounds');
 const startBtn = document.getElementById('startBtn');
 const pauseBtn = document.getElementById('pauseBtn');
-//let seconds = 10;
-
-/*
-pauseBtn.addEventListener("click", clearInterval);
-//const a = setInterval(CountDown, 1000);
-startBtn.addEventListener("click", setInterval(CountDown, 1000));
-*/
 
 function CountDown(seconds){
-    timer.textContent = seconds;
-    if (seconds < 0) {
+
+    timer.textContent = seconds; //print on the screen the current seconds value
+
+    if (seconds < 0) {  //check if time's up and if it is clear the memory and print "Time's Up!"
         clearInterval(intervalId);
         timer.textContent = "Time's up!";
+        return;
     }
 } 
-let seconds = 5; 
-const intervalId = setInterval(() => CountDown(seconds--), 1000); //CountDown(seconds--) first the function is called with the value seconds and after it is called seconds-- is performed
 
+function startCountDown(seconds){
+    //let seconds = 5; //initialize the amount of seconds to countdown
+
+    if(intervalId){ //clear the memory
+        clearInterval(intervalId);
+    }
+
+    //start the countdown
+    intervalId = setInterval(() => CountDown(seconds--), 1000); //CountDown(seconds--) first the function is called with the value seconds and after it is called seconds-- is performed
+
+}
+
+//click the button and call the startCountDown function
+startBtn.addEventListener("click",()=>startCountDown(15));
 
 /* NOTES! 
+for some reason if i don't write the calling of a function as an arrow function it calls it automatically without me pressing the button
  */
