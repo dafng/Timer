52d82ef55bd3f1d7680d76cb6e4ad53de9e587c4
Dafni Gk <dafnigkortsi@gmail.com>
2024-10-28 13:35:18 +0200

    made seconds variable global

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ const rounds = document.getElementById('rounds');
 const startBtn = document.getElementById('startBtn');
 const pauseBtn = document.getElementById('pauseBtn');
 
-function CountDown(seconds){
+function CountDown(currentseconds){
 
-    timer.textContent = seconds; //print on the screen the current seconds value
+    timer.textContent = currentseconds; //print on the screen the current seconds value
 
 
-    if (seconds < 0) {  //check if time's up and if it is clear the memory and print "Time's Up!"
+    if (currentseconds < 0) {  //check if time's up and if it is clear the memory and print "Time's Up!"
         clearInterval(intervalId);
         timer.textContent = "Time's up!";
         return;
     }
 } 
 
-function startCountDown(seconds){
+function startCountDown(initialseconds){
     //let seconds = 5; //initialize the amount of seconds to countdown
 
     if(intervalId){ //clear the memory
         clearInterval(intervalId);
     }
-
+    seconds = initialseconds;
     //start the countdown
     intervalId = setInterval(() => CountDown(seconds--), 1000); //CountDown(seconds--) first the function is called with the value seconds and after it is called seconds-- is performed
 
